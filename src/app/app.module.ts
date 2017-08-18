0b167c1f93384c098b0cec289d2e8478dc455e84
svadapalli <satya.aquarianadm@gmail.com>
2017-08-17 20:55:30 -0400

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
